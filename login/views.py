844e22ed11451e844557ef316f73709fbc66d1b4
Gabriel Rosa <81601748+gfreitasrosa@users.noreply.github.com>
2024-11-10 19:58:42 -0300

    Update views.py

--- login/views.py
+++ login/views.py
@@ -15,19 +15,22 @@ def login_view(request):
             data = json.loads(request.body)
             email = data.get('email')
             password = data.get('password')
-
+            print(1)
             if email is None or password is None:
                 return JsonResponse({'message': 'Email e senha são obrigatórios'}, status=400)
-
+                print(2)
             user = authenticate(request, username=email, password=password)
 
             if user is not None:
                 login(request, user)
+                print(3)
                 return JsonResponse({'message': 'Login bem-sucedido'}, status=200)
             else:
+                print(4)
                 return JsonResponse({'message': 'Credenciais inválidas'}, status=401)
 
         except json.JSONDecodeError:
+            print(5)
             return JsonResponse({'message': 'Erro no formato do JSON'}, status=400)
 
     return JsonResponse({'message': 'Método não permitido'}, status=405)
