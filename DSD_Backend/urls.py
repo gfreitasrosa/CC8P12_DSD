0d7baf32f2b378856f9d170360fdae2b2615808b
Gabriel Rosa <81601748+gfreitasrosa@users.noreply.github.com>
2024-11-12 12:33:49 -0300

    Update urls.py

--- DSD_Backend/urls.py
+++ DSD_Backend/urls.py
@@ -16,9 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('login.urls')),  # Inclua as rotas do app
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+# Configuração do Schema View (documentação)
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Minha API",
+        default_version='v1',
+        description="Documentação da minha API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contato@minhaapi.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
